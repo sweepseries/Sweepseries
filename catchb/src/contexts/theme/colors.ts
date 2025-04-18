919bc708e5606d502ea5ea29961fca6af70c43e7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-18 14:16:09 +0900

    remove unused color

--- catchb/src/contexts/theme/colors.ts
+++ catchb/src/contexts/theme/colors.ts
@@ -5,7 +5,6 @@ export type ThemeColorType = {
   background: string;
   backgroundGray: string;
   border: string;
-  logo: string;
   highEmphasis: string;
   mediumEmphasis: string;
   lowEmphasis: string;
@@ -18,7 +17,6 @@ export const lightColors: ThemeColorType = {
   background: "#FFFFFF",
   backgroundGray: "#F5F5F5",
   border: "#D9D9D9",
-  logo: "#083F25",
   highEmphasis: "#262626",
   mediumEmphasis: "#555555",
   lowEmphasis: "#9D9D9D",
@@ -31,7 +29,6 @@ export const darkColors: ThemeColorType = {
   background: "#FFFFFF", // #262626
   backgroundGray: "#F5F5F5", // #1A1A1A
   border: "#D9D9D9", // #9D9D9D
-  logo: "#083F25",
   highEmphasis: "#262626",
   mediumEmphasis: "#555555",
   lowEmphasis: "#9D9D9D",
