84894950fba4bde8956eeaf3aac424fbc36766e0
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-22 13:15:16 +0900

    Set timeout

--- catchb/src/services/app/initialize.ts
+++ catchb/src/services/app/initialize.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export async function initialize() {
   try {
-    const response = await axios.get("/v1/initialize/");
+    const response = await axios.get("/v1/initialize/", {
+      timeout: 3000,
+    });
 
     return response.data;
   } catch {
