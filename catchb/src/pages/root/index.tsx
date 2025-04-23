77195e8431236ee1db34de018a3c640ecd154839
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-04-23 15:47:10 +0900

    add userprofile page

---
+++ catchb/src/pages/root/index.tsx
@@ -0,0 +1,23 @@
+import { LandingPage } from "./landing/LandingPage";
+import { LoginPage } from "./login/LoginPage";
+import { RootLayout } from "./RootLayout";
+import { SignupLayout } from "./signup/SignupLayout";
+import { TermContents } from "./signup/Terms/TermContents";
+import { TermsAndConditions } from "./signup/Terms/Terms";
+import { UsernameEmail } from "./signup/UsernameEmail/UsernameEmail";
+import { Passwords } from "./signup/Passwords/Passwords";
+import { PhoneVerification } from "./signup/PhoneVerification/PhoneVerification";
+import { UserProfile } from "./signup/UserProfile/UserProfile";
+
+export {
+  LandingPage,
+  LoginPage,
+  RootLayout,
+  SignupLayout,
+  TermContents,
+  TermsAndConditions,
+  UsernameEmail,
+  Passwords,
+  PhoneVerification,
+  UserProfile,
+};
