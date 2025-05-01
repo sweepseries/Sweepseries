cb71d6c6b423e3a3caf12b96c9a1b19a92f33aa1
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-01 16:18:54 +0900

    add icons

--- catchb/src/06.shared/ui/Icons/AppIcon.tsx
+++ catchb/src/06.shared/ui/Icons/AppIcon.tsx
@@ -1,7 +1,15 @@
 import { SvgProps } from "react-native-svg";
 
+import CalendarNumber from "./files/calendar-number.svg";
+import CheckCircle from "./files/check-circle.svg";
 import ChevronDown from "./files/chevron-down.svg";
+import ChevronLeft from "./files/chevron-left.svg";
+import ChevronRight from "./files/chevron-right.svg";
 import ChevronUp from "./files/chevron-up.svg";
+import Desk from "./files/desk.svg";
+import Home from "./files/home.svg";
+import People from "./files/people.svg";
+import PersonCircle from "./files/person-circle.svg";
 
 interface Props {
   icon: string;
@@ -10,16 +18,26 @@ interface Props {
 }
 
 const iconMap: Record<string, React.FC<SvgProps>> = {
+  "calendar-number": CalendarNumber,
+  "check-circle": CheckCircle,
   "chevron-down": ChevronDown,
+  "chevron-left": ChevronLeft,
+  "chevron-right": ChevronRight,
   "chevron-up": ChevronUp,
+  desk: Desk,
+  home: Home,
+  people: People,
+  "person-circle": PersonCircle,
 };
 
-export const AppIcon = ({ icon, size = 20, color = "#000" }: Props) => {
+export const AppIcon = ({ icon, size = 24, color = "#000" }: Props) => {
   const IconComponent = iconMap[icon];
 
   if (!IconComponent) {
     return null;
   }
 
-  return <IconComponent width={size} height={size} color={color} />;
+  return (
+    <IconComponent width={size} height={size} fill={color} color={color} />
+  );
 };
