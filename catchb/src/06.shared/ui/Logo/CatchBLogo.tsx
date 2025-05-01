75c118c8d20e77be7be78f3b67bf9d579b57d2a1
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-01 17:05:25 +0900

    handle color

--- catchb/src/06.shared/ui/Logo/CatchBLogo.tsx
+++ catchb/src/06.shared/ui/Logo/CatchBLogo.tsx
@@ -5,5 +5,13 @@ interface Props {
 }
 
 export function CatchBMainLogo({ opacity = 1 }: Readonly<Props>) {
-  return <MainLogo width={160} height={160} fill="#083F25" opacity={opacity} />;
+  return (
+    <MainLogo
+      width={160}
+      height={160}
+      fill="#083F25"
+      color="#083F25"
+      opacity={opacity}
+    />
+  );
 }
