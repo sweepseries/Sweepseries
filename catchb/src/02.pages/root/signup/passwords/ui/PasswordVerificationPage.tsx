eb0901caf53d1cd078ca35f1d674062d03734f16
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-05 15:35:05 +0900

    add password check page

---
+++ catchb/src/02.pages/root/signup/passwords/ui/PasswordVerificationPage.tsx
@@ -0,0 +1,54 @@
+import {
+  PasswordsProvider,
+  usePasswordVerification,
+} from "../provider/PasswordsProvider";
+import { SignUpForm } from "@widgets/signupform";
+import { InputField } from "@features/signup/common";
+
+export function PasswordVerificationPage() {
+  return (
+    <PasswordsProvider>
+      <Components />
+    </PasswordsProvider>
+  );
+}
+
+function Components() {
+  const {
+    password,
+    password2,
+    passwordError,
+    isButtonActive,
+    setPassword,
+    setPassword2,
+    goToNextPage,
+  } = usePasswordVerification();
+
+  return (
+    <SignUpForm
+      title="비밀번호를 설정해주세요!"
+      subtitle={"영문+숫자+특수문자 조합으로\n8자리 이상 입력해주세요."}
+      buttonText="다음으로"
+      buttonOnPress={goToNextPage}
+      buttonDisabled={!isButtonActive}
+    >
+      <InputField
+        title="비밀번호"
+        value={password}
+        onChangeText={setPassword}
+        placeholder="영문+숫자+특수문자 조합으로 8자리 이상"
+        secureTextEntry
+        returnKeyType="next"
+      />
+      <InputField
+        title="비밀번호 확인"
+        value={password2}
+        onChangeText={setPassword2}
+        placeholder="비밀번호를 다시 입력해주세요."
+        secureTextEntry
+        returnKeyType="done"
+        errorMessage={passwordError}
+      />
+    </SignUpForm>
+  );
+}
