37be04e26b118c0c01815245fdee211ee4bc2c2b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-05 16:28:03 +0900

    format

--- catchb/src/02.pages/root/signup/phone/provider/contexts.ts
+++ catchb/src/02.pages/root/signup/phone/provider/contexts.ts
@@ -1,20 +1,20 @@
 import { createContext } from "react";
 
 export interface PhoneVerificationContextType {
-    name: string;
-    phoneNumberMiddle: string;
-    phoneNumberLast: string;
-    codeInput: string;
-    verified: boolean;
-    sent: boolean;
-    timer: number;
-    error: string;
-    setName: (name: string) => void;
-    setPhoneNumberMiddle: (middle: string) => void;
-    setPhoneNumberLast: (last: string) => void;
-    setCodeInput: (code: string) => void;
-    sendRequest: () => void;
-    checkCode: () => void;
+  name: string;
+  phoneNumberMiddle: string;
+  phoneNumberLast: string;
+  codeInput: string;
+  verified: boolean;
+  sent: boolean;
+  timer: number;
+  error: string;
+  setName: (name: string) => void;
+  setPhoneNumberMiddle: (middle: string) => void;
+  setPhoneNumberLast: (last: string) => void;
+  setCodeInput: (code: string) => void;
+  sendRequest: () => void;
+  checkCode: () => void;
 }
 
 export const PhoneVerificationContext = createContext<
