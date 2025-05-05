f6060d567885ca3effe692b5b870835389f7ae22
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-05 15:35:21 +0900

    add verify password api

---
+++ catchb/src/04.features/signup/verify-password/api/verify.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+import { ErrorResponse } from "@shared/api";
+
+interface CheckPasswordsVariables {
+  password: string;
+  password2: string;
+}
+
+async function checkPassword(
+  password: string,
+  password2: string
+): Promise<AxiosResponse<null>> {
+  return axios.post("/v1/check-password/", {
+    password,
+    password2,
+  });
+}
+
+export function usePasswordCheck() {
+  return useMutation<
+    AxiosResponse<null>,
+    AxiosError<ErrorResponse>,
+    CheckPasswordsVariables
+  >({
+    mutationFn: ({ password, password2 }) => checkPassword(password, password2),
+  });
+}
