4e1e61fa49b9d73c2a0e115ce28a661067a0f519
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-06 11:10:03 +0900

    add util setup

--- catchb/src/test-utils/renderer.tsx
+++ catchb/src/test-utils/renderer.tsx
@@ -1,12 +1,22 @@
 import { ReactElement, PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components/native";
 import { render } from "@testing-library/react-native";
 
-import { ColorsProvider } from "@shared/lib/colors";
+import { ColorsProvider, sampleColors } from "@shared/lib/colors";
+
+const queryClient = new QueryClient();
 
 export const renderWithProviders = (ui: ReactElement) => {
   function Wrapper({ children }: PropsWithChildren): JSX.Element {
-    return <ColorsProvider>{children}</ColorsProvider>;
+    return (
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={{ colors: sampleColors }}>
+          <ColorsProvider>{children}</ColorsProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    );
   }
 
-  return { ...render(ui, { wrapper: Wrapper }) };
+  return render(ui, { wrapper: Wrapper });
 };
