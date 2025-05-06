87448f79c1d6bcac3f04f2fba56e069ffd6227b0
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-06 18:09:22 +0900

    more global mocks

--- catchb/src/test-utils/jest.setup.tsx
+++ catchb/src/test-utils/jest.setup.tsx
@@ -1,14 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-//import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
-//import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
-jest.mock("expo-router", () => ({
-  router: {
-    back: jest.fn(),
-    push: jest.fn(),
-  },
-  useLocalSearchParams: jest.fn().mockReturnValue({ id: "1" }),
-}));
+jest.mock("expo-router", () => {
+  const { View } = jest.requireActual("react-native");
+
+  return {
+    Stack: Object.assign(
+      ({
+        children,
+        screenOptions,
+      }: {
+        children: React.ReactNode;
+        screenOptions: NativeStackNavigationOptions;
+      }) => (
+        <View>
+          {screenOptions.headerLeft && screenOptions.headerLeft({})}
+          {/* if screen.options.headerTitle is a function, call it with an empty object */}
+          {screenOptions.headerTitle &&
+          typeof screenOptions.headerTitle === "function"
+            ? screenOptions.headerTitle({ children: "" })
+            : screenOptions.headerTitle}
+          {children}
+        </View>
+      ),
+      {
+        Screen: ({ options }: { options?: NativeStackNavigationOptions }) => (
+          <View>
+            {options?.headerLeft && options.headerLeft({})}
+            {options?.headerTitle && options.headerTitle}
+          </View>
+        ),
+      }
+    ),
+    router: {
+      back: jest.fn(),
+      push: jest.fn(),
+      replace: jest.fn(),
+      canDismiss: jest.fn().mockReturnValue(true),
+      dismissAll: jest.fn(),
+    },
+    useLocalSearchParams: jest.fn().mockReturnValue({ id: "1" }),
+  };
+});
 jest.mock("react-native-skeleton-placeholder", () => {
   const { Text } = jest.requireActual("react-native");
   const Item = () => <Text>Loading Item</Text>;
@@ -26,16 +59,21 @@ jest.mock("@shared/lib/alert", () => ({
     showAlert: jest.fn(),
   })),
 }));
-jest.mock("@shared/lib/auth", () => ({
-  AuthProvider: ({ children }: { children: React.ReactNode }) => children,
-  useAuth: jest.fn(() => ({
-    saveLoginStatus: jest.fn(),
-    resetLoginStatus: jest.fn(),
-    user: null,
-    mode: "GUEST",
-    isAuthenticated: false,
-  })),
-}));
+jest.mock("@shared/lib/auth", () => {
+  const { sampleLoginData } = jest.requireActual("@shared/lib/auth");
+
+  return {
+    AuthProvider: ({ children }: { children: React.ReactNode }) => children,
+    useAuth: jest.fn(() => ({
+      saveLoginStatus: jest.fn(),
+      resetLoginStatus: jest.fn(),
+      user: null,
+      mode: "GUEST",
+      isAuthenticated: false,
+    })),
+    sampleLoginData,
+  };
+});
 jest.mock("@shared/lib/colors", () => {
   const { sampleColors } = jest.requireActual("@shared/lib/colors");
 
@@ -83,9 +121,16 @@ jest.mock("@shared/ui/Logo", () => ({
 jest.mock("@shared/ui/Selectors", () => ({
   Selector: () => null,
 }));
-jest.mock("@shared/ui/TextInput", () => ({
-  AuthTextInput: () => null,
-})); /*}
+jest.mock("@shared/ui/TextInput", () => {
+  const { TextInput } = jest.requireActual("react-native");
+
+  return {
+    AuthTextInput: TextInput,
+  };
+});
+
+jest.mock("@widgets/layouts/ui/horizontallogo.svg", () => () => null);
+jest.mock("@shared/ui/Logo/mainlogo.svg", () => () => null);
 
 /*
 jest.mock("expo-router", () => {
@@ -105,67 +150,11 @@ jest.mock("expo-router", () => {
   MockTopTabs.Screen = MockTopTabsScreen;
 
   return {
-    Stack: Object.assign(
-      ({
-        children,
-        screenOptions,
-      }: {
-        children: React.ReactNode;
-        screenOptions: NativeStackNavigationOptions;
-      }) => (
-        <View>
-          {screenOptions.headerLeft && screenOptions.headerLeft({})}
-          {/* if screen.options.headerTitle is a function, call it with an empty object */ /*}
-          {screenOptions.headerTitle &&
-          typeof screenOptions.headerTitle === "function"
-            ? screenOptions.headerTitle({ children: "" })
-            : screenOptions.headerTitle}
-          {children}
-        </View>
-      ),
-      {
-        Screen: ({ options }: { options?: NativeStackNavigationOptions }) => (
-          <View>
-            {options?.headerLeft && options.headerLeft({})}
-            {options?.headerTitle && options.headerTitle}
-          </View>
-        ),
-      }
-    ),
     Slot: () => <div data-testid="Slot" />,
     SplashScreen: {
       preventAutoHideAsync: jest.fn(),
       hideAsync: jest.fn(),
     },
-    Tabs: Object.assign(
-      ({
-        children,
-        screenOptions,
-      }: {
-        children: React.ReactNode;
-        screenOptions: BottomTabNavigationOptions;
-      }) => (
-        <View>
-          {screenOptions.headerLeft && screenOptions.headerLeft({})}
-          {/* if screen.options.headerTitle is a function, call it with an empty object */ /*}
-          {screenOptions.headerTitle &&
-          typeof screenOptions.headerTitle === "function"
-            ? screenOptions.headerTitle({ children: "" })
-            : screenOptions.headerTitle}
-          {children}
-        </View>
-      ),
-      {
-        Screen: ({ options }: { options?: BottomTabNavigationOptions }) => (
-          <View>
-            {options?.headerLeft && options.headerLeft({})}
-            {options?.headerTitle && options.headerTitle}
-            {options?.tabBarIcon &&
-              options.tabBarIcon({ focused: true, color: "", size: 0 })}
-          </View>
-        ),
-      }
-    ),
     Redirect: jest.fn(),
     router: {
       canGoBack: jest.fn().mockReturnValue(true),
@@ -183,23 +172,7 @@ jest.mock("expo-router", () => {
     withLayoutContext: jest.fn().mockReturnValue(MockTopTabs),
   };
 });
-jest.mock("react-native-gesture-handler", () => {
-  const { TouchableOpacity } = jest.requireActual("react-native");
-
-  return {
-    ...jest.requireActual("react-native-gesture-handler"),
-    GestureHandlerRootView: ({ children }: { children: React.ReactNode }) => (
-      <TouchableOpacity>{children}</TouchableOpacity>
-    ),
-  };
-});
 jest.mock("react-native-svg/css", () => ({
   SvgCssUri: "SvgCssUri",
-}));
-
-jest.mock("@react-native-kakao/user", () => ({
-  me: jest.fn(),
-  login: jest.fn(),
-  isLogined: jest.fn(),
 }));
 */
