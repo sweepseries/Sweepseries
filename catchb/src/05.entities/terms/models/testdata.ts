8ab1649fc91137e05fdf5d9c138c7f828fdcc128
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-06 14:47:19 +0900

    update test data

--- catchb/src/05.entities/terms/models/testdata.ts
+++ catchb/src/05.entities/terms/models/testdata.ts
@@ -3,14 +3,20 @@ import { TermsAndConditionsType } from "./types";
 export const sampleTerms: TermsAndConditionsType[] = [
   {
     id: 1,
-    title: "Sample Terms and Conditions",
-    content: "These are the sample terms and conditions.",
+    title: "Required Terms and Conditions",
+    content: "These are the sample required terms and conditions.",
     is_required: true,
   },
   {
     id: 2,
-    title: "Sample Privacy Policy",
-    content: "This is the sample privacy policy.",
+    title: "Required Terms and Conditions without content",
+    content: "These are the sample required terms and conditions.",
+    is_required: true,
+  },
+  {
+    id: 3,
+    title: "알림 수신 동의",
+    content: "",
     is_required: false,
   },
 ];
