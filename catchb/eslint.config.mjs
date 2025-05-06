a3711c2b50c0efd317dcd6830f183312d361d071
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-07 01:26:42 +0900

    fix eslint config

--- catchb/eslint.config.mjs
+++ catchb/eslint.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  pluginQuery.configs.recommended,
+  pluginQuery.configs["flat/recommended"],
   {
     rules: {
       "react/react-in-jsx-scope": "off",
