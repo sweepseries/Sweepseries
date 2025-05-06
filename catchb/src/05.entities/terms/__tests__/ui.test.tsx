a2073f9ceb6e9f0f0ece548a9518a78d1ca26b2b
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-06 14:50:58 +0900

    update tests

--- catchb/src/05.entities/terms/__tests__/ui.test.tsx
+++ catchb/src/05.entities/terms/__tests__/ui.test.tsx
@@ -1,9 +1,12 @@
-import { TermSimple, sampleTerms } from "@entities/terms";
+import { fireEvent } from "@testing-library/react-native";
+import * as Router from "expo-router";
+
+import { CheckAllTerms, TermSimple, sampleTerms } from "@entities/terms";
 import { renderWithProviders } from "@test-utils/renderer";
 
 describe("TermSimple", () => {
   it("should render correctly", () => {
-    renderWithProviders(
+    const { getByTestId } = renderWithProviders(
       <>
         <TermSimple
           term={sampleTerms[0]}
@@ -11,11 +14,28 @@ describe("TermSimple", () => {
           toggleCheck={jest.fn()}
         />
         <TermSimple
-          term={sampleTerms[1]}
+          term={sampleTerms[2]}
           isChecked={false}
           toggleCheck={jest.fn()}
         />
       </>
     );
+
+    fireEvent.press(getByTestId("right-1"));
+    expect(Router.router.push).toHaveBeenCalledWith(`/signup/terms/1`);
+  });
+});
+
+describe("CheckAllTerms", () => {
+  it("should render checked", () => {
+    const { getByTestId } = renderWithProviders(
+      <CheckAllTerms checked={true} toggle={jest.fn()} />
+    );
+
+    expect(getByTestId("toggle-all")).toBeTruthy();
+  });
+
+  it("should render unchecked", () => {
+    renderWithProviders(<CheckAllTerms checked={false} toggle={jest.fn()} />);
   });
 });
