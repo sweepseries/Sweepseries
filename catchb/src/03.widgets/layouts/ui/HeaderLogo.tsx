16cfd075676ed082e8b037d2e20d645e5bd06349
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-09 12:53:09 +0900

    bugfix

--- catchb/src/03.widgets/layouts/ui/HeaderLogo.tsx
+++ catchb/src/03.widgets/layouts/ui/HeaderLogo.tsx
@@ -1,8 +1,8 @@
 import HorizontalLogo from "./horizontallogo.svg";
 
 export function HeaderLeftLogo() {
-  const width = 40;
-  const height = width * 4.5;
+  const height = 30;
+  const width = height * 4.5;
 
   return <HorizontalLogo width={width} height={height} />;
 }
