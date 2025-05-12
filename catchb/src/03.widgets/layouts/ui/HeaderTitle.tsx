c00f9ae6ceb6e3284199e8eb8b1b49fc4f2047cf
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-12 09:59:42 +0900

    work android title

--- catchb/src/03.widgets/layouts/ui/HeaderTitle.tsx
+++ catchb/src/03.widgets/layouts/ui/HeaderTitle.tsx
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 import styled from "styled-components/native";
 
 interface Props {
@@ -5,6 +6,9 @@ interface Props {
 }
 
 export function HeaderTitle({ title }: Readonly<Props>) {
+  if (Platform.OS === "android") {
+    return <AndroidTitle>{title}</AndroidTitle>;
+  }
   return <Title>{title}</Title>;
 }
 
@@ -12,4 +16,8 @@ const Title = styled.Text`
   font-size: 18px;
   font-weight: 600;
   line-height: 24px;
+`;
+
+const AndroidTitle = styled(Title)`
+  margin-left: 12px;
 `;
