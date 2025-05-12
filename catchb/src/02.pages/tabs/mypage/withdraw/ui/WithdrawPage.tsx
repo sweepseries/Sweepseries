7450d0a4ee0ea6e7f7b4c0b1c3f2a9dc9b25dcc1
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-12 11:31:13 +0900

    add withdraw page

---
+++ catchb/src/02.pages/tabs/mypage/withdraw/ui/WithdrawPage.tsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
+import { router } from "expo-router";
+
+import { withdrawPageStyles } from "./styles";
+import {
+  WithdrawReasonToggle,
+  WithdrawReasonType,
+  useDeleteAccount,
+  withdrawReasons,
+} from "@features/auth/delete-account";
+import { useAlert } from "@shared/lib/alert";
+import { useAuth } from "@shared/lib/auth";
+import { useColors } from "@shared/lib/colors";
+import { TextButton } from "@shared/ui/Buttons";
+
+// TODO: 네이버 & 카카오 unlink
+
+export function WithdrawPage() {
+  const [selectedReasonId, setSelectedReasonId] = useState<number>(1);
+  const [reasonText, setReasonText] = useState<string>(
+    withdrawReasons[0].reason
+  );
+
+  const { showAlert } = useAlert();
+  const { resetLoginStatus, user } = useAuth();
+  const { colors } = useColors();
+  const { mutate: deleteAccount } = useDeleteAccount();
+  const styles = withdrawPageStyles(colors);
+
+  const toggleSelect = (reason: WithdrawReasonType) => {
+    setSelectedReasonId(reason.id);
+    if (reason.id === 7) {
+      setReasonText("");
+    } else {
+      setReasonText(reason.reason);
+    }
+  };
+
+  const goToTopPage = () => {
+    router.dismissAll();
+    router.replace("/");
+  };
+
+  const requestWithdraw = async () => {
+    if (user?.uuid === undefined) {
+      showAlert({
+        title: "오류",
+        message: "오류가 발생했습니다. 다시 시도해주세요.",
+      });
+      return;
+    }
+
+    deleteAccount(
+      {
+        reason: selectedReasonId,
+        reason_text: reasonText,
+        uuid: user?.uuid,
+      },
+      {
+        onSuccess: () => {
+          resetLoginStatus();
+          goToTopPage();
+        },
+        onError: () => {
+          showAlert({
+            title: "오류",
+            message: "오류가 발생했습니다. 다시 시도해주세요.",
+          });
+        },
+      }
+    );
+  };
+
+  const handleDeleteButtonPress = () => {
+    showAlert({
+      title: "회원탈퇴",
+      message: "정말로 회원탈퇴 하시겠습니까?",
+      onConfirm: requestWithdraw,
+      enableCancel: true,
+    });
+  };
+
+  const isCustomInput = selectedReasonId === 7;
+  const isButtonActive = selectedReasonId !== 7 || reasonText.length > 9;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>무엇이 불편하셨나요?</Text>
+        <Text style={styles.helper}>
+          탈퇴 이유를 알려주시면 불편 사항을 개선할 수 있도록 중요한 자료로
+          활용하겠습니다.
+        </Text>
+        <View style={styles.checkButtons}>
+          {withdrawReasons.map((reason) => (
+            <WithdrawReasonToggle
+              key={reason.id}
+              reason={reason}
+              onPress={toggleSelect}
+              isSelected={selectedReasonId === reason.id}
+            />
+          ))}
+        </View>
+        {isCustomInput && (
+          <TextInput
+            value={reasonText}
+            onChangeText={setReasonText}
+            placeholder="탈퇴 사유를 직접 입력해주세요. (최소 10자 이상)"
+            style={styles.input}
+            multiline
+            numberOfLines={4}
+          />
+        )}
+      </View>
+      <TextButton
+        text="탈퇴하기"
+        onPress={handleDeleteButtonPress}
+        active={isButtonActive}
+      />
+    </View>
+  );
+}
