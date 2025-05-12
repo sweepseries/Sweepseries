23f82088869603a1f776f009814294626d85ebe6
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-12 11:43:45 +0900

    add tests

--- catchb/src/02.pages/layouts/tabs/mypage/__tests__/MyPageLayout.test.tsx
+++ catchb/src/02.pages/layouts/tabs/mypage/__tests__/MyPageLayout.test.tsx
@@ -1,8 +1,25 @@
+import { Platform } from "react-native";
+
 import { MyPageLayout } from "@pages/layouts";
 import { renderWithProviders } from "@test-utils/renderer";
 
 describe("MyPageLayout", () => {
-  it("renders correctly", () => {
+  const realPlatform = Platform.OS;
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    Object.defineProperty(Platform, "OS", { value: realPlatform });
+  });
+
+  it("renders correctly in ios", () => {
+    Object.defineProperty(Platform, "OS", { value: "ios" });
+
+    renderWithProviders(<MyPageLayout />);
+  });
+
+  it("renders correctly in android", () => {
+    Object.defineProperty(Platform, "OS", { value: "android" });
+
     renderWithProviders(<MyPageLayout />);
   });
 });
