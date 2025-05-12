77a7a99cbe09d1705ebd866631efa10d42271c44
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-12 16:09:37 +0900

    create announcement app

---
+++ sweepserver/app/announcement/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from core.models import TimeStampedModel
+
+
+class Announcement(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    is_deleted = models.BooleanField(default=False)
+
+    objects = models.Manager()
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        db_table = "announcement"
+        verbose_name = "공지사항"
+        verbose_name_plural = "공지사항"
+        ordering = ["is_deleted", "-created_at"]
---
+++ sweepserver/app/announcement/views/__init__.py
@@ -0,0 +1,7 @@
+from .read_announcements import AnnouncementViewSet
+from .manage_announcements import AnnouncementAdminViewSet
+
+__all__ = [
+    "AnnouncementViewSet",
+    "AnnouncementAdminViewSet",
+]
