0a594b362bec024036e6e5372f6630f12d635e7c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-18 18:38:40 +0900

    add create inquiry api and test

---
+++ catchb/src/05.entities/inquiries/__tests__/createapi.test.tsx
@@ -0,0 +1,48 @@
+import { Text, View } from "react-native";
+import axios from "axios";
+
+import { sampleInquiries, useCreateInquiry } from "@entities/inquiries";
+import { renderWithProviders } from "@test-utils/renderer";
+import { fireEvent, waitFor } from "@testing-library/react-native";
+
+const MockCreateComponent = () => {
+  const { mutate, isSuccess } = useCreateInquiry();
+
+  const handleCreate = () => {
+    mutate({
+      title: "New Inquiry",
+      category: 0,
+      content: "This is a test inquiry.",
+      user: "user-uuid",
+    });
+  };
+
+  return (
+    <View>
+      <Text onPress={handleCreate}>Create Inquiry</Text>
+      {isSuccess && <Text>Inquiry Created</Text>}
+    </View>
+  );
+};
+
+describe("Inquiries Create API", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  it("should create an inquiry", async () => {
+    const mockResponse = {
+      data: sampleInquiries[0],
+    };
+
+    jest.spyOn(axios, "post").mockResolvedValueOnce(mockResponse);
+
+    const { getByText } = renderWithProviders(<MockCreateComponent />);
+
+    fireEvent.press(getByText("Create Inquiry"));
+
+    await waitFor(() => {
+      expect(getByText("Inquiry Created")).toBeTruthy();
+    });
+  });
+});
---
+++ catchb/src/05.entities/inquiries/api/createInquiry.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+import {
+  MemberInquiryCreatePostType,
+  GuestInquiryCreatePostType,
+  InquiryThreadType,
+} from "../models/types";
+import { ErrorResponse } from "@shared/api";
+
+async function createInquiry(
+  data: MemberInquiryCreatePostType | GuestInquiryCreatePostType
+): Promise<AxiosResponse<InquiryThreadType>> {
+  return await axios.post<InquiryThreadType>("/v1/inquiries/", data);
+}
+
+export function useCreateInquiry() {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    AxiosResponse<InquiryThreadType>,
+    AxiosError<ErrorResponse>,
+    MemberInquiryCreatePostType | GuestInquiryCreatePostType
+  >({
+    mutationFn: (data) => createInquiry(data),
+    onSuccess: (res) => {
+      queryClient.setQueryData<InquiryThreadType[]>(
+        ["inquiries"],
+        (old = []) => [res.data, ...old]
+      );
+    },
+  });
+}
