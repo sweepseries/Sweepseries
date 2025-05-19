3fd6f016f790089b577b0f5013d22ed0b7ff327d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-19 09:19:08 +0900

    Add tests

---
+++ catchb/src/04.features/inquiries/create-new-inquiry/__tests__/extra.test.tsx
@@ -0,0 +1,18 @@
+import { Text } from "react-native";
+import { render } from "@testing-library/react-native";
+
+import { useInquiryForm } from "../provider/InquiryFormProvider";
+
+const TestComponent = () => {
+  const { isOpen } = useInquiryForm();
+
+  return <Text>{isOpen ? "Active" : "Inactive"}</Text>;
+};
+
+describe("InquiryFormProvider", () => {
+  it("should throw an error if used outside of provider", () => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+
+    expect(() => render(<TestComponent />)).toThrow();
+  });
+});
