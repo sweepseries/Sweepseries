23bd6c661598168344c1853aad4c10627129639d
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 00:23:27 +0900

    finish user profile card

--- catchb/src/05.entities/users/__tests__/UserProfileCard.test.tsx
+++ catchb/src/05.entities/users/__tests__/UserProfileCard.test.tsx
@@ -1,13 +1,50 @@
+import { fireEvent } from "@testing-library/react-native";
+import { router } from "expo-router";
+
 import { UserProfileCard } from "@entities/users";
 import { sampleUserProfile } from "@shared/lib/auth";
 import { renderWithProviders } from "@test-utils/renderer";
 
+jest.mock("expo-image", () => {
+  const { View } = jest.requireActual("react-native");
+
+  return {
+    Image: () => <View testID="image" />,
+  };
+});
 jest.mock("expo-linear-gradient", () => ({
-  LinearGradient: ({ children }: {children: React.ReactNode}) => <>{children}</>,
+  LinearGradient: ({ children }: { children: React.ReactNode }) => (
+    <>{children}</>
+  ),
 }));
+jest.mock("../ui/files/default_profile.svg", () => {
+  const { View } = jest.requireActual("react-native");
+
+  return {
+    __esModule: true,
+    default: () => <View testID="default-profile" />,
+  };
+});
 
 describe("UserProfileCard", () => {
-  it("should render correctly", () => {
-    renderWithProviders(<UserProfileCard profile={{...sampleUserProfile}} />);
+  it("should render profile image correctly", () => {
+    const { getByTestId } = renderWithProviders(
+      <UserProfileCard profile={sampleUserProfile} />
+    );
+
+    expect(getByTestId("image")).toBeTruthy();
+
+    fireEvent.press(getByTestId("edit-profile"));
+    expect(router.push).toHaveBeenCalledWith("/mypage/profile");
+  });
+
+  it("should fall back to default profile", () => {
+    const { getByTestId } = renderWithProviders(
+      <UserProfileCard
+        profile={{ ...sampleUserProfile, profile_image: null }}
+      />
+    );
+
+    expect(getByTestId("default-profile")).toBeTruthy();
   });
 });
