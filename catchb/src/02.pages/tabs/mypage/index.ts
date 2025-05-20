3344e10a00caa6ad40e60bb14865a6a022cc8ae9
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 00:34:18 +0900

    add placeholder page

--- catchb/src/02.pages/tabs/mypage/index.ts
+++ catchb/src/02.pages/tabs/mypage/index.ts
@@ -3,4 +3,5 @@ export { MyPageMain } from "./main/ui/MyPage";
 export { AnnouncementsPage } from "./announcements/ui/AnnouncementsPage";
 export { FAQsPage } from "./faqs/ui/FAQsPage";
 export { InquiriesPage } from "./inquiries/ui/InquiriesPage";
+export { ProfileEditPage } from "./profile/ui/ProfileEditPage";
 export { WithdrawPage } from "./withdraw/ui/WithdrawPage";
