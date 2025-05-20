5b3420829946b827a6705610abcd724bfce9ca52
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 00:36:25 +0900

    add test for coverage

---
+++ catchb/src/04.features/auth/delete-account/__tests__/extra.test.tsx
@@ -0,0 +1,18 @@
+import { Text } from "react-native";
+import { render } from "@testing-library/react-native";
+
+import { useWithdrawSheet } from "../provider/WithdrawSheetProvider";
+
+const TestComponent = () => {
+  const { openSheet } = useWithdrawSheet();
+
+  return <Text onPress={openSheet}>Open Sheet</Text>;
+};
+
+describe("WithdrawSheetProvider", () => {
+  it("should throw an error if used outside of provider", () => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+
+    expect(() => render(<TestComponent />)).toThrow();
+  });
+});
