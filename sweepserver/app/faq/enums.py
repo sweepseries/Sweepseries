d6a3f1fdb60c9fe3220241ae24e5fee659ec43b2
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-20 17:55:37 +0900

    fix category options

--- sweepserver/app/faq/enums.py
+++ sweepserver/app/faq/enums.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class FAQCategoryChoices(models.IntegerChoices):
-    RESERVATIONS = 1, "예약"
-    EVENTS = 2, "이벤트"
-    ACADEMY = 3, "아카데미"
-    LESSONS = 4, "레슨"
-    PROMODE = 5, "프로모드"
-    OTHERS = 6, "기타"
+    EVENTS = 1, "이벤트"
+    ACADEMY = 2, "아카데미"
+    LESSONS = 3, "레슨"
+    PROMODE = 4, "프로모드"
+    OTHERS = 5, "기타"
