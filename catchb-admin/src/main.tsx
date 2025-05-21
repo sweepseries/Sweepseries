923352c22f8276c0e0860291811b584176a876a4
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 11:08:06 +0900

    initial axios setup

--- catchb-admin/src/main.tsx
+++ catchb-admin/src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
 
 import App from "./01.app/App";
+
+if (process.env.NODE_ENV === "production") {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+} else {
+  axios.defaults.baseURL = "http://localhost:8000";
+}
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  return config;
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
