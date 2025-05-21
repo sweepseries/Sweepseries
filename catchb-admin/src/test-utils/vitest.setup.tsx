5d7aa9fdc4d7fcb868e0739ad12f4b6a95974200
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-22 01:05:56 +0900

    add global mocks

--- catchb-admin/src/test-utils/vitest.setup.tsx
+++ catchb-admin/src/test-utils/vitest.setup.tsx
@@ -1,1 +1,47 @@
 import "@testing-library/jest-dom";
+import { vi } from "vitest";
+
+vi.mock("@shared/lib/auth", async () => {
+  const { AuthContext } = await vi.importActual("@shared/lib/auth");
+
+  return {
+    useAuth: vi.fn(() => ({
+      login: vi.fn(),
+      logout: vi.fn(),
+      isAuthenticated: false,
+    })),
+    AuthContext: AuthContext,
+  };
+});
+vi.mock("@shared/lib/colors", async () => {
+  const { ColorContext, ThemeColorType, light } = await vi.importActual(
+    "@shared/lib/colors"
+  );
+
+  return {
+    useColors: vi.fn(() => ({
+      colors: light,
+      toggleTheme: vi.fn(),
+    })),
+    ColorContext: ColorContext,
+    ThemeColorType: ThemeColorType,
+    light: light,
+    dark: light,
+  };
+});
+vi.mock("@shared/lib/navigation", async () => {
+  const { TabType, tabs } = await vi.importActual("@shared/lib/navigation");
+
+  return {
+    tabs: tabs,
+    TabType: TabType,
+    SidebarTab: () => <div>Mocked SidebarTab</div>,
+  };
+});
+
+vi.mock("@shared/ui/Buttons", () => ({
+  SidebarButton: () => <div>Mocked SidebarButton</div>,
+}));
+vi.mock("@shared/ui/Icons", () => ({
+  Logo: () => <div>Mocked Logo</div>,
+}));
