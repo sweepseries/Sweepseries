6b356ed8b4f5d30e3e4cc13e6e92721e321ae2ea
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 11:10:43 +0900

    fix bug

--- sweepserver/auth/user/views/auth.py
+++ sweepserver/auth/user/views/auth.py
@@ -1,4 +1,5 @@
 from typing import cast
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from django.utils import timezone
@@ -60,9 +61,12 @@ def get_tokens_for_user(user):
 def process_response(request, response):
     user_agent = request.headers.get("X-Sweep-Platform", "")
 
-    if user_agent not in ["sweep/mobile"]:
-        ## remove refresh token
-        response.data["refresh"] = ""
+    if user_agent == "sweep/mobile":
+        refresh = response.cookies.get("refresh").value
+        response.delete_cookie("access")
+        response.delete_cookie("refresh")
+
+        response.data["refresh"] = refresh
 
     return response
 
