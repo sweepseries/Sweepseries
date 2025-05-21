b371757e9f39377de0cfde9766996da881eb0f79
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 13:29:34 +0900

    add test code

---
+++ catchb-admin/src/06.shared/lib/auth/__tests__/useAuth.test.tsx
@@ -0,0 +1,40 @@
+import { describe, expect, it, vi } from "vitest";
+import { render } from "@testing-library/react";
+
+import { AuthContext, useAuth } from "@shared/lib/auth";
+
+const MockComponent = () => {
+  const { isAuthenticated } = useAuth();
+
+  return <div>{isAuthenticated ? "Authenticated" : "Not Authenticated"}</div>;
+};
+
+const MockProvider = ({ children }: { children: React.ReactNode }) => {
+  const authContextValue = {
+    isAuthenticated: false,
+    login: vi.fn(),
+    logout: vi.fn(),
+  };
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+describe("useAuth", () => {
+  it("should return isAuthenticated as false when not authenticated", () => {
+    expect(() => render(<MockComponent />)).toThrow();
+  });
+
+  it("should return isAuthenticated as true when authenticated", () => {
+    const { getByText } = render(
+      <MockProvider>
+        <MockComponent />
+      </MockProvider>
+    );
+
+    expect(getByText("Not Authenticated")).toBeInTheDocument();
+  });
+});
