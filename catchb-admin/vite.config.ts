fe1101c4c49c4293376e797e8bfbaeb68499fe3c
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-05-21 11:07:35 +0900

    add svgr config and fix bug

--- catchb-admin/vite.config.ts
+++ catchb-admin/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@pages": path.resolve(__dirname, "src/02.pages"),
-      "@widgets": path.resolve(__dirname, "src/03.processes"),
+      "@widgets": path.resolve(__dirname, "src/03.widgets"),
       "@features": path.resolve(__dirname, "src/04.features"),
       "@entities": path.resolve(__dirname, "src/05.entities"),
       "@shared": path.resolve(__dirname, "src/06.shared"),
