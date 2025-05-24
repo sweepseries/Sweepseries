2f7bef861094bddefe65f6e9a3a8e32caddd2389
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-24 14:38:55 +0900

    disable strictmode

--- catchb-admin/src/main.tsx
+++ catchb-admin/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
 
@@ -11,8 +10,4 @@ axios.interceptors.request.use(function (config) {
   return config;
 });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
