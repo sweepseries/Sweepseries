b1ad33ba6330e014859bd86440280fb3a49a200f
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-25 18:23:05 +0900

    refactor tests

--- sweepserver/app/terms/tests/test_user_apis.py
+++ sweepserver/app/terms/tests/test_user_apis.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import TermsAndConditions, TermsAndConditionsHistory
+from ..models import TermsAndConditions, TermsAndConditionsHistory
 
 
 class TermsAndConditionsAPITestCase(APITestCase):
