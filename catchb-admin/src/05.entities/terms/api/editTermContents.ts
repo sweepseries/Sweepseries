68d22f70c76a1dcaae40c08ed5e63affe9c44343
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-30 02:39:13 +0900

    improve code

--- catchb-admin/src/05.entities/terms/api/editTermContents.ts
+++ catchb-admin/src/05.entities/terms/api/editTermContents.ts
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosError, type AxiosResponse } from "axios";
 
-import type { UpdateContentsFormValues } from "../models/types";
+import type {
+  AdminTermsAndConditionsDetailType,
+  UpdateContentsFormValues,
+} from "../models/types";
 import type { APIErrorResponse } from "@shared/api";
 
 async function editTermContents(
   termId: number,
   data: UpdateContentsFormValues
-): Promise<AxiosResponse<void>> {
-  return await axios.patch<void>(
+): Promise<AxiosResponse<AdminTermsAndConditionsDetailType>> {
+  return await axios.patch<AdminTermsAndConditionsDetailType>(
     `/api/admin/v1/terms/${termId}/content/`,
     data
   );
@@ -17,13 +20,16 @@ export function useEditTermContents(termId: number) {
   const client = useQueryClient();
 
   return useMutation<
-    AxiosResponse<void>,
+    AxiosResponse<AdminTermsAndConditionsDetailType>,
     AxiosError<APIErrorResponse>,
     UpdateContentsFormValues
   >({
     mutationFn: (data) => editTermContents(termId, data),
-    onSuccess: () => {
-      client.invalidateQueries({ queryKey: ["termDetails", termId] });
+    onSuccess: (response: AxiosResponse<AdminTermsAndConditionsDetailType>) => {
+      client.setQueryData<AdminTermsAndConditionsDetailType>(
+        ["termDetails", termId],
+        () => response.data
+      );
     },
   });
 }
