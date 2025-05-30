266f0945cff315d3e29e336fdbabbf04a3197ad5
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-05-30 19:12:30 +0900

    Add is_important field

--- sweepserver/app/announcement/models.py
+++ sweepserver/app/announcement/models.py
@@ -7,6 +7,7 @@ class Announcement(TimeStampedModel):
     title = models.CharField(max_length=255)
     content = models.TextField()
 
+    is_important = models.BooleanField(default=False)
     is_deleted = models.BooleanField(default=False)
 
     objects = models.Manager()
@@ -18,4 +19,4 @@ class Announcement(TimeStampedModel):
         db_table = "announcement"
         verbose_name = "공지사항"
         verbose_name_plural = "공지사항"
-        ordering = ["is_deleted", "-created_at"]
+        ordering = ["-is_important", "is_deleted", "-created_at"]
