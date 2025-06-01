80166e9ea4326ca5d5cf567b830436f116a53a09
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-02 02:05:06 +0900

    remove unused import

--- sweepserver/app/announcement/tests/test_user_apis.py
+++ sweepserver/app/announcement/tests/test_user_apis.py
@@ -1,8 +1,6 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from ..models import Announcement
-
 class AnnouncementAPITestCase(APITestCase):
     """
     공지사항 API 테스트
