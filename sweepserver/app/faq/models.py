8db1c4dd72629116b56b8dda86ceaa4c368d3be8
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-02 21:12:29 +0900

    switch category from enum to model

--- sweepserver/app/faq/models.py
+++ sweepserver/app/faq/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
-from .enums import FAQCategoryChoices
+
+class FAQCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    color = models.CharField(max_length=7, default="#000000")
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "faq_category"
+        verbose_name = "FAQ 카테고리"
+        verbose_name_plural = "FAQ 카테고리"
+        ordering = ["id"]
 
 
 class FAQ(models.Model):
-    category = models.IntegerField(
-        choices=FAQCategoryChoices.choices, default=FAQCategoryChoices.OTHERS
+    category = models.ForeignKey(
+        FAQCategory, on_delete=models.CASCADE, related_name="faqs"
     )
     question = models.CharField(max_length=255)
     answer = models.TextField()
@@ -15,8 +29,7 @@ class FAQ(models.Model):
     objects = models.Manager()
 
     def __str__(self):
-        category_display = dict(FAQCategoryChoices.choices).get(self.category)
-        return f"{category_display} - {self.question}"
+        return f"{self.category.name} - {self.question}"
 
     class Meta:
         db_table = "faq"
