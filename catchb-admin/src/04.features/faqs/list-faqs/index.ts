53016a20e85c09e16e8bd674cedcfc8271290b95
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-04 03:19:06 +0900

    Refactor

--- catchb-admin/src/04.features/faqs/list-faqs/index.ts
+++ catchb-admin/src/04.features/faqs/list-faqs/index.ts
@@ -2,6 +2,5 @@ export { useFAQList } from "./hooks/useFAQList";
 
 export { FAQListProvider } from "./providers/FAQListProvider";
 
-export { FAQCategoryTabs } from "./ui/FAQCategoryTabs";
 export { FAQTableContents } from "./ui/FAQTableContents";
 export { FAQTableHeader } from "./ui/FAQTableHeader";
