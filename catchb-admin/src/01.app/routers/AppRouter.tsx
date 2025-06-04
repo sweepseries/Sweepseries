5750f2dfe122710ad5d2ebe54717d37fde22a5d6
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-04 17:56:08 +0900

    route new page

--- catchb-admin/src/01.app/routers/AppRouter.tsx
+++ catchb-admin/src/01.app/routers/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 import { AnnouncementsContainer } from "@pages/announcements";
 import { FAQsContainer } from "@pages/faqs";
+import { InquiriesContainer } from "@pages/inquiries";
 import { LoginPage } from "@pages/login";
 import { TermsContainer } from "@pages/terms";
 import { NotFoundPage } from "@widgets/fallback/notfound";
@@ -16,6 +17,7 @@ export function AppRouter() {
           <Route index element={<Navigate to="home" replace />} />
           <Route path="announcements/*" element={<AnnouncementsContainer />} />
           <Route path="faqs/*" element={<FAQsContainer />} />
+          <Route path="inquiries/*" element={<InquiriesContainer />} />
           <Route path="terms/*" element={<TermsContainer />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
