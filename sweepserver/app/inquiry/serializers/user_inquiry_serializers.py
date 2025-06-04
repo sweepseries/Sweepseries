8894d31ecf8500b7b7ecf0e141dcad7458d84a39
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-04 17:37:17 +0900

    update serializer

--- sweepserver/app/inquiry/serializers/user_inquiry_serializers.py
+++ sweepserver/app/inquiry/serializers/user_inquiry_serializers.py
@@ -1,7 +1,6 @@
 from django.db.transaction import atomic
 from rest_framework import serializers
 
-from ..enums import InquiryCategoryChoices, InquiryMessageTypeChoices
 from ..models import InquiryThread, InquiryMessage
 
 
@@ -19,8 +18,8 @@ class UserInquiryReadSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation["category"] = instance.get_category_display()
-        representation["status"] = instance.get_status_display()
+        representation["category"] = instance.category.name
+        representation["status"] = instance.status.name
         return representation
 
     def get_is_updated(self, obj):
@@ -45,13 +44,11 @@ class UserInquiryWriteSerializer(serializers.ModelSerializer):
         write_only=True,
         error_messages={"blank": "내용을 입력해주세요."},
     )
-    category = serializers.ChoiceField(
-        choices=InquiryCategoryChoices.choices,
-        required=False,
-        error_messages={
-            "blank": "질문 구분을 선택해주세요.",
-            "invalid_choice": "잘못된 요청입니다.",
-        },
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=InquiryThread.objects.all(),
+        source="category",
+        write_only=True,
+        error_messages={"required": "카테고리를 선택해주세요."},
     )
     name = serializers.CharField(max_length=255, required=False)
     email = serializers.EmailField(required=False)
@@ -97,7 +94,6 @@ class UserInquiryWriteSerializer(serializers.ModelSerializer):
             inquiry_thread = InquiryThread.objects.create(**validated_data)
             InquiryMessage.objects.create(
                 thread=inquiry_thread,
-                sender=InquiryMessageTypeChoices.USER,
                 content=content,
             )
 
