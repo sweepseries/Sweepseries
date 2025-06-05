8234618fa1f4b22c1ba8676d552a7fb7aa336c26
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-05 17:28:36 +0900

    improve loading ui

--- catchb-admin/src/03.widgets/fallback/loading/ui/LoadingTabs.tsx
+++ catchb-admin/src/03.widgets/fallback/loading/ui/LoadingTabs.tsx
@@ -4,17 +4,26 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 export function LoadingTabs() {
   return (
-    <Horizontal>
-      <Skeleton height={32} width={80} />
-      <Skeleton height={32} width={80} />
-      <Skeleton height={32} width={80} />
-    </Horizontal>
+    <Wrapper>
+      <Horizontal>
+        <Skeleton height={32} width={80} />
+        <Skeleton height={32} width={80} />
+        <Skeleton height={32} width={80} />
+      </Horizontal>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+`;
 
 const Horizontal = styled.div`
   display: flex;
   flex-direction: row;
   padding: 8px 12px;
   gap: 8px;
+
+  background-color: ${({ theme }) => theme.colors.background700};
+  border-radius: 8px;
 `;
