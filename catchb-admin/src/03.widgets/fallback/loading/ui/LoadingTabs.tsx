b697357f6092df3bf41d9411b26411d1a0185aab
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-05 16:05:37 +0900

    minor style fix

--- catchb-admin/src/03.widgets/fallback/loading/ui/LoadingTabs.tsx
+++ catchb-admin/src/03.widgets/fallback/loading/ui/LoadingTabs.tsx
@@ -5,9 +5,9 @@ import "react-loading-skeleton/dist/skeleton.css";
 export function LoadingTabs() {
   return (
     <Horizontal>
-      <Skeleton height={36} width={80} />
-      <Skeleton height={36} width={80} />
-      <Skeleton height={36} width={80} />
+      <Skeleton height={32} width={80} />
+      <Skeleton height={32} width={80} />
+      <Skeleton height={32} width={80} />
     </Horizontal>
   );
 }
@@ -17,7 +17,4 @@ const Horizontal = styled.div`
   flex-direction: row;
   padding: 8px 12px;
   gap: 8px;
-
-  background-color: ${({ theme }) => theme.colors.background500};
-  border-radius: 8px;
 `;
