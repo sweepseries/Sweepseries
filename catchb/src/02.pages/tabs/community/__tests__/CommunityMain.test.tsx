a74cf603b74b817b2599dbe9d768b58c2262a623
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-06-11 11:58:37 +0900

    apply pager view

--- catchb/src/02.pages/tabs/community/__tests__/CommunityMain.test.tsx
+++ catchb/src/02.pages/tabs/community/__tests__/CommunityMain.test.tsx
@@ -1,27 +1,52 @@
-import { fireEvent, waitFor } from "@testing-library/react-native";
+import { act, fireEvent, waitFor } from "@testing-library/react-native";
 
 import { CommunityMain } from "@pages/tabs/community";
 import { CommunityProvider } from "@entities/community";
+import { sampleColors } from "@shared/lib/colors";
 import { renderWithProviders } from "@test-utils/renderer";
 
 describe("커뮤니티 게시글 목록 페이지", () => {
-  it("Top Tabs: 누르면, 해당 탭 게시글 목록 표기", async () => {
+  it("Top Tabs: 누르거나 스크롤 하면, 해당 탭 게시글 목록 표기", async () => {
     const { getByTestId } = renderWithProviders(
       <CommunityProvider>
         <CommunityMain />
       </CommunityProvider>
     );
 
-    await waitFor(() => {
-      fireEvent.press(getByTestId("community-tab-덕아웃"));
-    });
-
+    // Test Tab Press
     await waitFor(() => {
       fireEvent.press(getByTestId("community-tab-드래프트"));
     });
+    expect(getByTestId("community-tab-text-덕아웃")).toHaveStyle({
+      color: sampleColors.lowEmphasis,
+    });
+    expect(getByTestId("community-tab-text-드래프트")).toHaveStyle({
+      color: sampleColors.primary,
+    });
+    expect(getByTestId("community-tab-text-마켓")).toHaveStyle({
+      color: sampleColors.lowEmphasis,
+    });
 
+    // Test Page Scroll
+    const pagerView = getByTestId("community-pager-view");
+    act(() => {
+      pagerView.props.onPageScroll({
+        nativeEvent: { position: 2, offset: 0.5 },
+      });
+      pagerView.props.onPageSelected({
+        nativeEvent: { position: 2 },
+      });
+    });
     await waitFor(() => {
-      fireEvent.press(getByTestId("community-tab-마켓"));
+      expect(getByTestId("community-tab-text-덕아웃")).toHaveStyle({
+        color: sampleColors.lowEmphasis,
+      });
+      expect(getByTestId("community-tab-text-드래프트")).toHaveStyle({
+        color: sampleColors.lowEmphasis,
+      });
+      expect(getByTestId("community-tab-text-마켓")).toHaveStyle({
+        color: sampleColors.primary,
+      });
     });
   });
 });
--- catchb/src/02.pages/tabs/community/main/CommunityMain.tsx
+++ catchb/src/02.pages/tabs/community/main/CommunityMain.tsx
@@ -1,5 +1,64 @@
+import { useEffect, useRef } from "react";
+import { Animated, Dimensions, Text, View } from "react-native";
+import PagerView, {
+  PagerViewOnPageScrollEvent,
+  PagerViewOnPageSelectedEvent,
+} from "react-native-pager-view";
+
+import { communityMainStyles } from "./_styles";
 import { PostListTabs } from "@features/posts/list-posts";
+import { useCommunity } from "@entities/community";
+
+const { width: screenWidth } = Dimensions.get("window");
 
 export function CommunityMain() {
-  return <PostListTabs />;
+  const { forums, activeForum, setActiveForum } = useCommunity();
+  const ref = useRef<PagerView>(null);
+  const translateX = useRef(new Animated.Value(0)).current;
+  const tabWidth = screenWidth / forums.length;
+
+  const onPageScroll = (event: PagerViewOnPageScrollEvent) => {
+    const { position, offset } = event.nativeEvent;
+    translateX.setValue((position + offset) * tabWidth);
+  };
+
+  const onPageSelected = (event: PagerViewOnPageSelectedEvent) => {
+    setActiveForum(forums[event.nativeEvent.position]);
+  };
+
+  useEffect(() => {
+    if (!activeForum) return;
+
+    const activeIndex = forums.findIndex(
+      (forum) => forum.id === activeForum.id
+    );
+    ref.current?.setPage(activeIndex);
+  }, [activeForum, forums]);
+
+  if (!activeForum) return null;
+
+  return (
+    <View style={communityMainStyles.flex}>
+      <PostListTabs translateX={translateX} />
+      <PagerView
+        style={communityMainStyles.flex}
+        initialPage={activeForum.id - 1}
+        onPageScroll={onPageScroll}
+        onPageSelected={onPageSelected}
+        ref={ref}
+        testID="community-pager-view"
+      >
+        {forums.map((forum) => (
+          <View
+            key={forum.id}
+            style={communityMainStyles.flex}
+            testID={`community-forum-${forum.name}`}
+          >
+            <Text style={{ fontSize: 24 }}>{forum.name}</Text>
+            {/* Here you can render the posts for the forum */}
+          </View>
+        ))}
+      </PagerView>
+    </View>
+  );
 }
