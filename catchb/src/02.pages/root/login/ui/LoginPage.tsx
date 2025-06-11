da8267ae2c9cd8582dadf4829c83ffbf2ec81f27
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-06-11 20:05:13 +0900

    wrap login page with keyboard wrapper

--- catchb/src/02.pages/root/login/ui/LoginPage.tsx
+++ catchb/src/02.pages/root/login/ui/LoginPage.tsx
@@ -7,6 +7,7 @@ import { CatchBLoginForm } from "@features/auth/catchb-login";
 import { FindPassword } from "@features/auth/find-password";
 import { FindUsername } from "@features/auth/find-username";
 import { useColors } from "@shared/lib/colors";
+import { KeyboardWrapper } from "@shared/lib/keyboard";
 import { TextButton } from "@shared/ui/Buttons";
 import { VerticalDivider } from "@shared/ui/Dividers";
 
@@ -19,23 +20,25 @@ export function LoginPage() {
   };
 
   return (
-    <View style={styles.container}>
-      <AuthHeader />
-      <CatchBLoginForm />
-      <View style={styles.troubleShootWrapper}>
-        <FindUsername />
-        <VerticalDivider />
-        <FindPassword />
+    <KeyboardWrapper padding={36}>
+      <View style={styles.container}>
+        <AuthHeader />
+        <CatchBLoginForm />
+        <View style={styles.troubleShootWrapper}>
+          <FindUsername />
+          <VerticalDivider />
+          <FindPassword />
+        </View>
+        <View style={styles.backButtonWrapper}>
+          <TextButton
+            text="돌아가기"
+            onPress={goBackToLandingPage}
+            backgroundColor={colors.background}
+            color={colors.lowEmphasis}
+            borderColor={colors.lowEmphasis}
+          />
+        </View>
       </View>
-      <View style={styles.backButtonWrapper}>
-        <TextButton
-          text="돌아가기"
-          onPress={goBackToLandingPage}
-          backgroundColor={colors.background}
-          color={colors.lowEmphasis}
-          borderColor={colors.lowEmphasis}
-        />
-      </View>
-    </View>
+    </KeyboardWrapper>
   );
 }
