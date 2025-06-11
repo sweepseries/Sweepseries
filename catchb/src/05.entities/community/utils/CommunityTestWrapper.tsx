f1cbe6b87d127cc6bc87fe916eabcf2794829689
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-06-11 17:06:39 +0900

    add test wrapper

---
+++ catchb/src/05.entities/community/utils/CommunityTestWrapper.tsx
@@ -0,0 +1,32 @@
+import { CommunityContext, CommunityContextType } from "../models/contexts";
+import {
+  sampleCommunityProfiles,
+  sampleCommunityForums,
+} from "../models/testdata";
+
+const defaultCommunityContext: CommunityContextType = {
+  activeProfile: sampleCommunityProfiles[0],
+  switchProfile: jest.fn(),
+  profiles: sampleCommunityProfiles,
+  forums: sampleCommunityForums,
+  activeForum: sampleCommunityForums[0],
+  setActiveForum: jest.fn(),
+};
+
+interface Props {
+  children: React.ReactNode;
+  override?: Partial<CommunityContextType>;
+}
+
+export function CommunityTestWrapper({
+  children,
+  override = {},
+}: Readonly<Props>) {
+  return (
+    <CommunityContext.Provider
+      value={{ ...defaultCommunityContext, ...override }}
+    >
+      {children}
+    </CommunityContext.Provider>
+  );
+}
