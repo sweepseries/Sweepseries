a09734918c3bd9ded1e244a502a59555333e9cc7
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-13 04:07:22 +0900

    mock storage

--- sweepserver/community/post/tests/test_apis.py
+++ sweepserver/community/post/tests/test_apis.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from auth.user.models import User
 from community.profiles.models import CommunityProfile
 from core.tests.utils import CatchBAPITestCase
@@ -23,18 +25,22 @@ class PostAPITestCase(CatchBAPITestCase):
             "content": "This is a test post content.",
         }
 
-    def test_create_post_success(self):
+    @patch("django.core.files.storage.default_storage.save")
+    def test_create_post_success(self, mock_save):
         """
         게시글 생성 성공 테스트.
         """
+        mock_save.return_value = "test.png"
         self.client.force_authenticate(user=self.normal_user)
         response = self.client.post(self.url, data=self.data)
         self.assertEqual(response.status_code, 201)
 
-    def test_create_post_with_image(self):
+    @patch("django.core.files.storage.default_storage.save")
+    def test_create_post_with_image(self, mock_save):
         """
         이미지 파일을 포함한 게시글 생성 테스트.
         """
+        mock_save.return_value = "test.png"
         self.client.force_authenticate(user=self.normal_user)
         data = {
             **self.data,
