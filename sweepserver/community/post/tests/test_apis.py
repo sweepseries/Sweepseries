f8917b537e514b32b278e2e9948f485b9861c251
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-13 04:00:34 +0900

    Add tests

--- sweepserver/community/post/tests/test_apis.py
+++ sweepserver/community/post/tests/test_apis.py
@@ -1,14 +1,18 @@
+from auth.user.models import User
+from community.profiles.models import CommunityProfile
 from core.tests.utils import CatchBAPITestCase
 
-from community.profiles.models import CommunityProfile
 
 class PostAPITestCase(CatchBAPITestCase):
     """
     게시글 API 테스트 케이스.
     """
 
+    fixtures = CatchBAPITestCase.fixtures + ["data/initial/forum.json"]
+
     def setUp(self):
         super().setUp()
+        self.admin = User.objects.get(username="admin")
         self.author_profile = CommunityProfile.objects.get(user=self.normal_user)
         self.url = "/api/v1/posts/"
         self.data = {
@@ -26,3 +30,26 @@ class PostAPITestCase(CatchBAPITestCase):
         self.client.force_authenticate(user=self.normal_user)
         response = self.client.post(self.url, data=self.data)
         self.assertEqual(response.status_code, 201)
+
+    def test_create_post_with_image(self):
+        """
+        이미지 파일을 포함한 게시글 생성 테스트.
+        """
+        self.client.force_authenticate(user=self.normal_user)
+        data = {
+            **self.data,
+            "image_files": [
+                self.uploaded_image_png,
+            ],
+        }
+        response = self.client.post(self.url, data=data, format="multipart")
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_post_unauthenticated(self):
+        """
+        입력받은 프로필과 현재 로그인한 사용자가 일치하지 않을 때
+        """
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["error"], "잘못된 요청입니다.")
--- sweepserver/core/tests/utils.py
+++ sweepserver/core/tests/utils.py
@@ -1,4 +1,7 @@
+from io import BytesIO
+from PIL import Image as PilImage
 from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APITestCase
 
 from auth.user.models import User
@@ -17,6 +20,17 @@ class AdminPageAPITestCase(APITestCase):
         self.admin_page = settings.ADMIN_PAGE_URL
 
 
+def generate_test_image_file():
+    image_file = BytesIO()
+    pil_image = PilImage.new("RGB", (100, 100), color="red")
+    pil_image.save(image_file, format="PNG")
+    image_file.seek(0)
+
+    return SimpleUploadedFile(
+        "test1.png", image_file.getvalue(), content_type="image/png"
+    )
+
+
 class CatchBAPITestCase(APITestCase):
     """
     테스트용 CatchB API 테스트 케이스.
@@ -26,3 +40,4 @@ class CatchBAPITestCase(APITestCase):
 
     def setUp(self):
         self.normal_user = User.objects.get(username="testuser")
+        self.uploaded_image_png = generate_test_image_file()
