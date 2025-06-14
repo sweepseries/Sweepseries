b3239dd84c50988c4cb66eee98d32ffef76df99c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-15 04:16:30 +0900

    simplify loading component

--- catchb/src/04.features/posts/list-posts/ui/_loading.tsx
+++ catchb/src/04.features/posts/list-posts/ui/_loading.tsx
@@ -82,56 +82,6 @@ export function LoadingPostsList() {
           />
           <SkeletonPlaceholder.Item width="100%" height={28} borderRadius={4} />
         </SkeletonPlaceholder.Item>
-        <SkeletonPlaceholder.Item width="100%" height={1} />
-        <SkeletonPlaceholder.Item width="100%" gap={4}>
-          <SkeletonPlaceholder.Item
-            width="10%"
-            height={16}
-            borderRadius={4}
-            marginVertical={8}
-          />
-          <SkeletonPlaceholder.Item
-            flexDirection="row"
-            justifyContent="space-between"
-            gap={8}
-          >
-            <SkeletonPlaceholder.Item flex={1} gap={4}>
-              <SkeletonPlaceholder.Item
-                width="100%"
-                height={24}
-                borderRadius={4}
-                marginBottom={4}
-              />
-              <SkeletonPlaceholder.Item
-                width="100%"
-                height={16}
-                borderRadius={4}
-              />
-              <SkeletonPlaceholder.Item
-                width="100%"
-                height={16}
-                borderRadius={4}
-              />
-              <SkeletonPlaceholder.Item
-                width="40%"
-                height={16}
-                borderRadius={4}
-              />
-            </SkeletonPlaceholder.Item>
-            <SkeletonPlaceholder.Item
-              width={100}
-              height={100}
-              borderRadius={8}
-            />
-          </SkeletonPlaceholder.Item>
-          <SkeletonPlaceholder.Item
-            width="10%"
-            height={16}
-            borderRadius={4}
-            marginVertical={4}
-          />
-          <SkeletonPlaceholder.Item width="100%" height={28} borderRadius={4} />
-        </SkeletonPlaceholder.Item>
       </SkeletonPlaceholder.Item>
     </SkeletonPlaceholder>
   );
