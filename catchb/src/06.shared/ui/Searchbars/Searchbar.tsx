72c0fbb02c4c244615c06dab19a2fb86ab95d326
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-14 18:26:03 +0900

    add search icon

--- catchb/src/06.shared/ui/Searchbars/Searchbar.tsx
+++ catchb/src/06.shared/ui/Searchbars/Searchbar.tsx
@@ -1,6 +1,12 @@
 import styled, { DefaultTheme } from "styled-components/native";
 
 import { useColors } from "@shared/lib/colors";
+import { AppIcon } from "@shared/ui/Icons";
+
+/**
+ * 기본 검색바 컴포넌트.
+ * 오른쪽에 돋보기 아이콘이 있는 검색 입력창.
+ */
 
 interface Props {
   placeholder: string;
@@ -16,10 +22,12 @@ export function Searchbar({ placeholder, value, onChange }: Readonly<Props>) {
       <Input
         placeholder={placeholder}
         placeholderTextColor={colors.lowEmphasis}
+        placeholderTextSize={4}
         value={value}
         onChangeText={onChange}
         enterKeyHint="search"
       />
+      <AppIcon icon="search" size={14} color={colors.primary} />
     </Container>
   );
 }
@@ -29,13 +37,15 @@ const Container = styled.View`
   align-items: center;
   justify-content: space-between;
   padding: 8px 12px;
+  gap: 8px;
   border: 1px solid
     ${({ theme }: { theme: DefaultTheme }) => theme.colors.border};
-  border-radius: 8px;
+  border-radius: 4px;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.05);
 `;
 
 const Input = styled.TextInput`
   flex: 1;
-  font-size: 16px;
+  font-size: 14px;
   color: ${({ theme }: { theme: DefaultTheme }) => theme.colors.highEmphasis};
 `;
