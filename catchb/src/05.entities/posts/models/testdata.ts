7285665855f31af21b4f6c7724bb79cda415b202
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-06-17 02:44:28 +0900

    fix testdata

--- catchb/src/05.entities/posts/models/testdata.ts
+++ catchb/src/05.entities/posts/models/testdata.ts
@@ -20,6 +20,10 @@ export const samplePostDetail: PostDetailType = {
   title: "샘플 게시글 제목",
   content: "샘플 게시글 내용입니다. 이곳에 게시글의 내용을 작성합니다.",
   images: [{ id: 1, image: "https://placehold.co/400" }],
+  num_views: 100,
+  num_comments: 10,
+  num_likes: 5,
+  created_at: "2023-10-01T12:00:00Z",
 };
 
 export const samplePostSimple: PostSimpleType = {
